2766c70a0e083da5a12973e4a3cee17695271975
Skrypit <fordemt2@gmail.com>
2022-08-10 23:36:58 +0300

    Fix V1__migration

--- out/production/resources/db/migration/V1__create_tables.sql
+++ out/production/resources/db/migration/V1__create_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS developers (
-    developer_id INT AUTO_INCREMENT PRIMARY KEY,
+    developer_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     age INT NOT NULL,
@@ -7,58 +7,58 @@ CREATE TABLE IF NOT EXISTS developers (
 );
 
 CREATE TABLE IF NOT EXISTS skills (
-    skill_id INT AUTO_INCREMENT PRIMARY KEY,
+    skill_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     language VARCHAR(100),
     level VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS projects (
-    project_id INT AUTO_INCREMENT PRIMARY KEY,
+    project_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     project_name VARCHAR(100),
     type VARCHAR(100),
     description VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS companies (
-    company_id INT AUTO_INCREMENT PRIMARY KEY,
+    company_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     company_name VARCHAR(100),
     residence VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS customers (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id BIGINT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     email VARCHAR(100)
 );
 
 CREATE TABLE IF NOT EXISTS developers_projects(
-developer_id  INT NOT NULL,
-project_id  INT NOT NULL,
+developer_id  BIGINT NOT NULL,
+project_id  BIGINT NOT NULL,
 PRIMARY KEY (developer_id, project_id),
 FOREIGN KEY(project_id) REFERENCES projects(project_id),
 FOREIGN KEY (developer_id) REFERENCES developers(developer_id)
 );
 
 CREATE TABLE IF NOT EXISTS developer_skills(
-developer_id  INT NOT NULL,
-skill_id  INT NOT NULL,
+developer_id  BIGINT NOT NULL,
+skill_id  BIGINT NOT NULL,
 PRIMARY KEY (developer_id, skill_id),
 FOREIGN KEY(skill_id) REFERENCES skills(skill_id),
 FOREIGN KEY (developer_id) REFERENCES developers(developer_id)
 );
 
 CREATE TABLE IF NOT EXISTS companies_projects(
-company_id INT NOT NULL,
-project_id  INT NOT NULL,
+company_id BIGINT NOT NULL,
+project_id  BIGINT NOT NULL,
 PRIMARY KEY (company_id, project_id),
 FOREIGN KEY(project_id) REFERENCES projects(project_id),
 FOREIGN KEY (company_id) REFERENCES companies(company_id)
 );
 
 CREATE TABLE IF NOT EXISTS customers_projects(
-customer_id  INT NOT NULL,
-project_id  INT NOT NULL,
+customer_id  BIGINT NOT NULL,
+project_id  BIGINT NOT NULL,
 PRIMARY KEY (customer_id, project_id),
 FOREIGN KEY(project_id) REFERENCES projects(project_id),
 FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
